8df5d86b8103d461298dd639fd4c446ce9f7f795
Ali-Aljufairi <ali.redha.ali@gmail.com>
2024-01-28 19:37:12 +0300

    edit search

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -1,12 +1,13 @@
-// Footer.tsx
-
 import React from 'react';
-import { GitHub, Linkedin } from 'react-feather';
+
+import { GitHub, Linkedin, Instagram } from 'react-feather';
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white p-4">
+
       <div className="container mx-auto flex justify-between items-center">
+
         <p>&copy; 2024 Ali Aljufairi</p>
         <div className="flex space-x-4">
           <a
@@ -27,7 +28,17 @@ const Footer: React.FC = () => {
             <Linkedin size={20} />
             <span className="ml-2"></span>
           </a>
+          <a
+            href="https://www.instagram.com/alooy657/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center hover:text-gray-400"
+          >
+            <Instagram size={20} />
+            <span className="ml-2"></span>
+          </a>
         </div>
+
       </div>
     </footer>
   );
