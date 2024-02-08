cf022d3db45436072fe368be03596f7778752ea1
Ali-Aljufairi <ali.redha.ali@gmail.com>
2024-02-09 00:22:06 +0300

    edit Tailwind CSS file to change h-4 to h-3 height

--- components/misc/header.tsx
+++ components/misc/header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
           <ul className="flex grow justify-end flex-wrap items-center">
             <li className="flex items-center">
               <h1 className='text-Pufr-300 mr-0'>⌘k</h1>
-              <button className="w-4 h-4 my-auto mx-2 border-black" aria-label="Search" onClick={() => setSearching(!searching)} disabled={searching}>
+              <button className="w-4 h-3 my-auto mx-2 border-black" aria-label="Search" onClick={() => setSearching(!searching)} disabled={searching}>
                 <svg className="w-4 h-4 fill-current text-Pufr-300" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                   <path d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5zM15.707 14.293L13.314 11.9a8.019 8.019 0 01-1.414 1.414l2.393 2.393a.997.997 0 001.414 0 .999.999 0 000-1.414z" />
                 </svg>
