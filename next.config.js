c955c8ff458aa9083db802ee0347cfc63aa3af43
Ali Aljufairi <ali.redha.ali@gmail.com>
2024-03-15 16:42:24 +0300

    Try Documentation

--- next.config.js
+++ next.config.js
@@ -2,23 +2,12 @@ const path = require('path');
 
 module.exports = {
   output: 'standalone',
-  outputFileTracing: {
-    root: path.join(__dirname, '/'),
-    includeRoot: true,
-    globDirectory: '.',
-    externalDirectories: [
-      'node_modules',
-      '.next',
-      'out',
-      '.git',
-      '.gitignore',
-      '.github/images/logo.gif',
-      '.obsidian/icons/font-awesome-brands.zip',
-      'common_md/attachments',
-      // Add any other directories or files you want to exclude
-    ],
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+    outputFileTracingExcludes: {
+      '/.github': true,
+    },
   },
-
   async redirects() {
     return [
       {
