363836746d342b0a703fb3c49b6decfd4ff7cb10
Ali Aljufairi <ali.redha.ali@gmail.com>
2024-03-15 16:28:17 +0300

    Changed the `next.config.js` file

--- next.config.js
+++ next.config.js
@@ -2,14 +2,22 @@ const path = require('path');
 
 module.exports = {
   output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '/'),
-  outputFileTracingExcludePatterns: [
-    'node_modules',
-    '.next',
-    'out',
-    '.git',
-    '.gitignore'
-  ],
+  outputFileTracing: {
+    root: path.join(__dirname, '/'),
+    includeRoot: true,
+    globDirectory: '.',
+    externalDirectories: [
+      'node_modules',
+      '.next',
+      'out',
+      '.git',
+      '.gitignore',
+      '.github/images/logo.gif',
+      '.obsidian/icons/font-awesome-brands.zip',
+      'common_md/attachments',
+      // Add any other directories or files you want to exclude
+    ],
+  },
 
   async redirects() {
     return [
