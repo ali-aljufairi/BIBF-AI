38979de9513bdda36ece9ee1ba1708c24b14eb1f
Ali Aljufairi <ali.redha.ali@gmail.com>
2024-03-15 16:13:24 +0300

    Changed next config and excluded some files from the build process

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
+const path = require('path');
+
 module.exports = {
+  output: 'standalone',
+  outputFileTracing: {
+    root: path.join(__dirname, '/'),
+    includeRoot: true,
+    globDirectory: '.',
+    externalDirectories: [
+      // Exclude the following directories from the build
+      'node_modules',
+      '.next',
+      'out',
+      '.git', // Exclude the .git directory
+      '.gitignore', // Exclude the .gitignore file
+    ],
+  },
+
   async redirects() {
     return [
       {
-        source: "/",
-        destination: "/BIBF-AI",
+        source: '/',
+        destination: '/BIBF-AI',
         permanent: true,
       },
     ];
