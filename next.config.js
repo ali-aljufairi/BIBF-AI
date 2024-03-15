d2135b353e41c912e0207899d7e9f36cd9b1e7a8
Ali Aljufairi <ali.redha.ali@gmail.com>
2024-03-15 16:21:46 +0300

    Fix Bolean value in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,19 +2,14 @@ const path = require('path');
 
 module.exports = {
   output: 'standalone',
-  outputFileTracing: {
-    root: path.join(__dirname, '/'),
-    includeRoot: true,
-    globDirectory: '.',
-    externalDirectories: [
-      // Exclude the following directories from the build
-      'node_modules',
-      '.next',
-      'out',
-      '.git', // Exclude the .git directory
-      '.gitignore', // Exclude the .gitignore file
-    ],
-  },
+  outputFileTracingRoot: path.join(__dirname, '/'),
+  outputFileTracingExcludePatterns: [
+    'node_modules',
+    '.next',
+    'out',
+    '.git',
+    '.gitignore'
+  ],
 
   async redirects() {
     return [
